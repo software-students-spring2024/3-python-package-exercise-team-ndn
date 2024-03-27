5a01a2731a6b80aa993c63875e758def3b69b465
nz792 <nz2061@nyu.edu>
2024-03-26 21:17:42 -0400

    implemented remaining functions

--- src/ndnGeometryCalc/packageCode.py
+++ src/ndnGeometryCalc/packageCode.py
@@ -5,3 +5,17 @@ def sphere(radius):
     surfaceArea = 4 * math.pi * (radius**2)
     return volume, surfaceArea
 
+def cylinder(radius, height):
+    volume = math.pi * (radius**2) * height
+    surfaceArea = (2 * math.pi * radius) * (height + radius)
+    return volume, surfaceArea
+
+def rectangularPrism(length, width, height):
+    volume = length * width * height
+    surfaceArea = 2 * ((length*width) + (length*height) + (width*height))
+    return volume, surfaceArea
+
+def cone(radius, height):
+    volume = math.pi * (radius**2) * height / 3
+    surfaceArea = math.pi * radius * (radius + math.sqrt((height**2)+(radius**2)))
+    return volume, surfaceArea
